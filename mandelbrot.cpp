cd12eb39db53708eaf9a6c05d8771f04ce237af6
ConnorWarren <connorqwarren@gmail.com>
2026-09-19 20:59:00 -0400

    Add basic rendering and fix mandelbrot set calculation

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1,19 +1,22 @@
 #include <complex>
 #include <memory>
 #include <SDL3/SDL.h>
+#include <iostream>
 
 // Squared escape radius
 const double ESCAPE_RADIUS = 4;
-const int MAX_ITERATIONS = 500;
+const int MAX_ITERATIONS = 1000;
 
 using WindowPtr = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
 using RendererPtr = std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)>;
 
 bool insideMandelbrot(std::complex<double> c) {
     int i = 0;
+    std::complex<double> z = {0, 0};
     while (i < MAX_ITERATIONS) {
-        if (std::norm(c) > ESCAPE_RADIUS)
+        if (std::norm(z) > ESCAPE_RADIUS)
             return false;
+        z = z * z + c;
         i++;
     }
     return true;
@@ -21,20 +24,34 @@ bool insideMandelbrot(std::complex<double> c) {
 
 class MandelbrotRegion {
 private:
-    std::complex<double> topLeft;
-    std::complex<double> bottomRight;
+    std::complex<double> m_topLeft;
+    std::complex<double> m_bottomRight;
 public:
-    MandelbrotRegion() : topLeft{-1.0, 1.0}, bottomRight{1.0, -1.0} {}
+    MandelbrotRegion() : m_topLeft{-2.0, 1.0}, m_bottomRight{1.0, -1.0} {}
     MandelbrotRegion(std::complex<double> topLeft, std::complex<double> bottomRight) 
-        : topLeft{topLeft}, bottomRight{bottomRight} {}
+        : m_topLeft{topLeft}, m_bottomRight{bottomRight} {}
+    std::complex<double> topLeft() {
+        return m_topLeft;
+    }
+    std::complex<double> bottomRight() {
+        return m_bottomRight;
+    }
+    double realRange() {
+        return m_bottomRight.real() - m_topLeft.real();
+    }
+    double imagRange() {
+        return m_topLeft.imag() - m_bottomRight.imag();
+    }
 };
 
 class MandelbrotWindow {
 private:
+    int windowWidth = 600;
+    int windowHeight = 400;
     WindowPtr window{
         SDL_CreateWindow(
             "Mandelbrot Renderer",
-            600, 400, SDL_WINDOW_RESIZABLE
+            windowWidth, windowHeight, SDL_WINDOW_RESIZABLE
         ),
         SDL_DestroyWindow
     };
@@ -46,8 +63,10 @@ private:
         SDL_DestroyRenderer
     };
     MandelbrotRegion region;
+    std::unique_ptr<int[]> pixelData;
     bool running;
-    
+    // Keeps track of visible rendering updates
+    bool updateRendering;
     /*
     * Handle SDL events in a given frame. Returns true unless 
     * a quit event is trigged.
@@ -63,18 +82,47 @@ private:
         }
         return true;
     }
+    void computeSet() {
+        int resolution = windowWidth * windowHeight;
+        pixelData = std::make_unique<int[]>(resolution);
+        for (int i = 0; i < resolution; i++) {
+            int row = i / windowWidth;
+            int col = i % windowWidth;
+            double real = region.realRange() / windowWidth * col + region.topLeft().real();
+            double imag = region.imagRange() / windowHeight * row + region.bottomRight().imag();
+            std::complex<double> point{
+                real, imag
+            };
+            pixelData[i] = (int) insideMandelbrot(point);
+       }
+    }
     void render() {
-        
+        SDL_RenderClear(renderer.get());
+        for (int i = 0; i < windowWidth * windowHeight; i++) {
+            if (pixelData[i])
+                SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+            else
+                SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 255);
+            int row = i / windowWidth;
+            int col = i % windowWidth;
+            SDL_RenderPoint(renderer.get(), row, col);
+        }
+        SDL_RenderPresent(renderer.get());
     }
 public:
     void run(MandelbrotRegion region = MandelbrotRegion()) {
         running = true;
+        updateRendering = true;
         this->region = region;
         while (running) {
             if (!handleEvents())
                 // user quit, don't render next frame.
                 break;
-            render();
+            if (updateRendering) {
+                computeSet();
+                render();
+                updateRendering = false;
+            }
         }
     }
 };
